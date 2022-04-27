69a9acc77cc5d9f9dfd633dec6315570842cf8f8
Miguel <miguel.danto@gmail.com>
2022-04-27 01:15:46 -0500

    definiendo portada

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,7 @@ import Portada from "./components/Portada.vue"
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,body{
+  margin: 0;
 }
 </style>
--- src/components/Portada.vue
+++ src/components/Portada.vue
@@ -1,7 +1,9 @@
 <template>
-  <img src="../assets/itecgeo4.png" alt=""/>
-
   <div class="mapa1" ref="mapa1"></div>
+  <div class="logo-container">
+    <img src="../assets/itecgeo4.png" alt=""/>
+  </div>
+  
 </template>
 
 <script setup>
@@ -10,18 +12,33 @@
   const mapa1 = ref(null)
 
   onMounted(() => {
-    console.log(mapa1)
+    
     const map = new maplibregl.Map({
       container: mapa1._value,
-      style: 'https://api.maptiler.com/maps/toner/style.json?key=jrcIzdt1OrHwCiQa3qeS', // stylesheet location
-      center: [-99.65631,19.2902], // starting position [lng, lat]
-      zoom: 12, // starting zoom,
-      pitch:50
+      style: 'https://api.maptiler.com/maps/topo/style.json?key=jrcIzdt1OrHwCiQa3qeS', // stylesheet location
+      center: [0,0], // starting position [lng, lat]
+      zoom: 1, // starting zoom,
+      pitch:0,
+      bearing: 0
     });
 
     map.on("load",function() {
+      map.flyTo({
+        center: [-99.65631,19.2902],
+        zoom: 12, // starting zoom,
+        pitch:45,
+        bearing: 180,
+        speed: 0.2,
+        curve: 1,
+        easing(t) {
+          return t;
+        }
+      })
+    });
+
+    map.on('zoomend', function() {
       rotateCamera(0)
-    })
+    });
 
     function rotateCamera(timestamp) {
       // clamp the rotation between 0 -360 degrees
@@ -35,6 +52,22 @@
 
 <style>
 .mapa1{
-  height: 400px;
+  height: 100vh;
+}
+
+.logo-container{
+  position: fixed;
+  display: flex;
+  top:0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: #4141412e;
+}
+
+.logo-container img{
+  height: 75px;
 }
 </style>
